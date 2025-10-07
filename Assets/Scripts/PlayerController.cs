7e67821eb43d8e11e83d5145943fd0e29959b058
GarhiSDGs_Diamond <sdgs.diamond@garhijapan.com>
2025-10-07 15:29:07 +0900

    Initial Unity project commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour {
+    public float moveSpeed = 5f;
+    public float jumpForce = 12f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private bool jumpPressed;
+    private bool jumpHeld;
+    private bool jumpCutApplied;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private bool isGrounded;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+        // 地面判定
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    void FixedUpdate(){
+        // 横移動
+        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
+
+        // ジャンプ開始
+        if (jumpPressed && isGrounded){
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            jumpPressed = false;
+            jumpCutApplied = false; // 新しいジャンプなのでリセット
+        }
+
+        // 可変ジャンプ処理
+        if (!jumpHeld && !jumpCutApplied && rb.linearVelocity.y > 0){
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+            jumpCutApplied = true; // 一度だけ適用
+        }
+    }
+
+    // Invoke Unity Events 用
+    public void OnMove(InputAction.CallbackContext context){
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context){
+        if (context.started){
+            jumpPressed = true;
+            jumpHeld = true;
+        }
+        else if (context.canceled){
+            jumpHeld = false;
+        }
+    }
+
+}
