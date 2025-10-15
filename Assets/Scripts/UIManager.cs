ef46bb45f027f877e93415ad4cf903e76ccb87a2
GarhiSDGs_Diamond <sdgs.diamond@garhijapan.com>
2025-10-15 15:30:33 +0900

    8th-2nd commit 25/10/15 15:30 -> Unity project commit

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 
 public class UIManager : MonoBehaviour {
+    private Canvas canvas;
     public static UIManager Instance { get; private set; }
     [SerializeField] private TextMeshProUGUI coinText;
 
@@ -13,11 +14,30 @@ public class UIManager : MonoBehaviour {
             Debug.LogWarning("重複したUIManagerが検出されたため破棄");
             Destroy(gameObject);
         }
+        canvas = GetComponent<Canvas>();
     }
 
     private void Start(){
         if (CoinManager.Instance != null){
             UpdateCoinUI(CoinManager.Instance.GetTotalCoins());
+        }
+    }
+    private void OnEnable(){
+        // シーン遷移時にもカメラを再設定
+        AssignCamera();
+    }
+
+    private void OnLevelWasLoaded(int level){
+        AssignCamera();
+    }
+
+    private void AssignCamera(){
+        if (canvas.renderMode == RenderMode.ScreenSpaceCamera){
+            var mainCam = Camera.main;
+            if (mainCam != null){
+                canvas.worldCamera = mainCam;
+                Debug.Log($"Canvas に {mainCam.name} を再設定しました");
+            }
         }
     }
 
